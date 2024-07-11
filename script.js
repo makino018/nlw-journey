196ad54d8de74b34492fc5f37bb0e14c71f8928c
mASKEINO <pedromakino53@gmail.com>
2024-07-11 10:30:11 -0300

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const atividade = {
+    nome:"Almoço",
+    data: new Date("2024-07-08 10:00"),
+    finalizada: true
+}
+
+// Lista, arrays, vetor []
+
+const atividades = [
+    atividade,
+    {
+        nome: 'Academia em grupo',
+        data: new Date("2024-07-09 12:00"),
+        finalizada: false
+    },
+    {
+        nome: 'Gaming session',
+        data: new Date("2024-07-09 16:00"),
+        finalizada: true
+    },
+]
+
+
+const criarItemDeAtividade = (atividade) => {
+    let input = '<input type="checkbox" '
+
+    if(atividade.finalizada) {
+        input  += 'checked'
+    }
+    
+    input +=  '>'
+
+    return `
+          <div>
+            ${input}
+            <span>${atividade.nome}</span>
+            <time>${atividade.data}</time>
+        </div>
+        `  
+}
+
+
+const section = document.querySelector('section')
+for (let atividade of atividades) {
+    section.innerHTML += criarItemDeAtividade(atividade)
+}
